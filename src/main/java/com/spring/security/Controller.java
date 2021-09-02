17c15391d815df1c51538ee10cd3883d1298f58d
gisungchoi <chlrltjd159@naver.com>
2021-09-02 17:35:32 +0900

    feat : security 기본 설정
    - 직접 form loginPage를 만들 수도 있다
    - 실패시, 성공시 각기 다른 핸들러를 호출한다
    - 핸들러 내부에는 인증객체가 들어있다

---
+++ src/main/java/com/spring/security/Controller.java
@@ -0,0 +1,18 @@
+package com.spring.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index(){
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
+}
--- src/main/java/com/spring/security/SecurityConfig.java
+++ src/main/java/com/spring/security/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -12,9 +21,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //인가 정책
-        http.authorizeRequests().anyRequest().authenticated();
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
 
-        //인증 정책
-        http.formLogin();
+        //인증 정책 /form 방식
+        http.formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication = 인증 객체" +authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        System.out.println("exception " +exception.getMessage());
+                        response.sendRedirect("/login");
+
+                    }
+                })
+                .permitAll();
     }
 }
