e7d2e86f6aa38fab4121e7c116e50b6b63d9f308
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2021-10-12 21:25:24 +0900

    feat : 메소드 방식 공부

--- sample2/corespringsecurity/src/main/java/io/security/corespringsecurity/controller/HomeController.java
+++ sample2/corespringsecurity/src/main/java/io/security/corespringsecurity/controller/HomeController.java
@@ -1,14 +1,19 @@
 package io.security.corespringsecurity.controller;
 
 
+import io.security.corespringsecurity.domain.AccountDto;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
-	
+
+	@PreAuthorize("hasRole('ROLE_USER') and #account.user == principal.username")
 	@GetMapping(value="/")
-	public String home() throws Exception {
+	public String home(AccountDto accountDto, Principal principal) throws Exception {
 		return "home";
 	}
 
